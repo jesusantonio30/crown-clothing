3bf7731ab9fe45f20666514b8ed4357e758cfdec
Jesus Morales <morales.jesusant@gmail.com>
2025-08-16 08:24:18 -0700

    Added initial components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,19 @@
+import CategoryItem from './components/category-item/category-item.component';
+import CategoriesArr from './utils/categories/CategoriesArr'
+import './categories.styles.scss'
 
 const App = () => {
 
-  return (
-    <div className="categories-container">
+    return (
+        <div className="categories-container">
 
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+            {CategoriesArr.map((category) => (
+            <CategoryItem key={category.id} category={category}/>
+            ))}
+
         </div>
-      </div>
-
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className="category-container">
-        {/* <img src="" alt="" /> */}
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-    </div>
-  )
+    )
 }
 
 export default App
+
